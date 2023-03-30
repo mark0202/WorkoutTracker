617fd78c58bae921a9ae8283b4865d3d7429df83
Mark Fitzpatrick <markfitz0202@gmail.com>
2023-03-30 08:48:24 +0200

    CsvFile

--- src/com/exercises/javaproject/WorkoutTracker.java
+++ src/com/exercises/javaproject/WorkoutTracker.java
@@ -98,7 +98,7 @@ public class WorkoutTracker
                 backstrokeDuration = backstrokeDuration + 2;     // 2 mins longer for next backstroke workout
                 backstrokeDistance = backstrokeDistance + 20;   // 20m further for next backstroke workout
 
-               break;
+                break;
             case 2:
                 workout = new SwimmingWorkout(workoutId,butterflyDuration,butterflyDistance,personId,SwimmingType.BUTTERFLY);
                 workoutList.add(workout);
@@ -106,7 +106,7 @@ public class WorkoutTracker
 
                 butterflyDuration = butterflyDuration + 1;     // 1 min longer
                 butterflyDistance = butterflyDistance + 15;     // 15m further
-               break;
+                break;
             case 3:
                 workout = new BikingWorkout(workoutId,mountainDuration,mountainDistance,personId,BikingType.MOUNTAIN);
                 workoutList.add(workout);
@@ -115,7 +115,7 @@ public class WorkoutTracker
                 mountainDuration = mountainDuration + 10;    // 10 mins
                 mountainDistance = mountainDistance + 1500; // 1.5k
 
-               break;
+                break;
             case 4:
                 workout = new BikingWorkout(workoutId,roadDuration,roadDistance,personId,BikingType.ROAD);
                 workoutList.add(workout);
